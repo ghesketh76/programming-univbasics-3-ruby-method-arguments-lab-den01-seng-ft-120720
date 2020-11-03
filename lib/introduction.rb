4bd6c27caf66be68e1d7a8f54c337f8ab0213a1c
Grant Hesketh <ghesketh76@gmail.com>
2020-11-03 17:38:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,11 @@ end
 
 introduction ("grant")
 
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to prgoram in #{language}."
+end
 
+introduction_with_language ("grant","Ruby")
 
 
 
